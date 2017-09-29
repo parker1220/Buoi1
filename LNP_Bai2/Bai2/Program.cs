83dcb1e92723a8021cfb50e317624f0a7fceea43
parker1220 <nguyenphucluc1220@gmail.com>
2017-09-29 22:09:57 +0700

    HoTroPhanGiai

---
+++ LNP_Bai2/Bai2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ten mien: ");
+            String host = Console.ReadLine();
+            IPHostEntry ipHE = Dns.GetHostEntry(host);
+            Console.WriteLine("HostName: {0}", ipHE.HostName);
+
+            foreach (string s in ipHE.Aliases)
+            {
+                Console.WriteLine("\t+Aliases: {0} \n", s);
+            }
+            foreach (IPAddress i in ipHE.AddressList)
+            {
+                Console.WriteLine("\t+ IP: {0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
