ae98faa7b57dda83ac52b707634e61c198d18541
parker1220 <nguyenphucluc1220@gmail.com>
2017-09-29 22:08:01 +0700

    PhanTichIP

---
+++ LNP_Bai1/Bai1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            Console.WriteLine("Dia chi IP cua may la:" + myIP);
+            Console.ReadKey();
+        }
+    }
+}
